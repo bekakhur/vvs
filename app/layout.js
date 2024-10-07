05665fc3bf6bfe3081d516c6ca201bef7692b907
Mac <mac@MacBook-Pro-Mac.local>
2024-10-07 12:21:17 +0300

    theme fix

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import HeaderC from "@/components/HeaderC";
 import Header from "@/components/Header";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,7 +23,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="overflow-hidden bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-300">
+      <Head>
+        {/*Android/Chrome */}
+        <meta name="theme-color" content="#000000" />
+
+        {/*iOS Safari */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </Head>
+      <body className="bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-300">
         <HeaderC />
         <div>{children}</div>
       </body>
