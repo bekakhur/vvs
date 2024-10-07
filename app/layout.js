4ff2973e0ddbf36bf94e9ceeb8ca61cafd420973
Mac <mac@MacBook-Pro-Mac.local>
2024-10-07 13:53:16 +0300

    navbar theme fix

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import HeaderC from "@/components/HeaderC";
 import Header from "@/components/Header";
-import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,22 +17,17 @@ const geistMono = localFont({
 export const metadata = {
   title: "VVS",
   description: "VVS",
+  themeColor: "#1f2937",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        {/*Android/Chrome */}
-        <meta name="theme-color" content="#000000" />
-
-        {/*iOS Safari */}
+      <head>
+        {/* Meta tag for iOS Safari status bar */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-      </Head>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </head>
       <body className="bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-300">
         <HeaderC />
         <div>{children}</div>
