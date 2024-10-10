494c29b8714131924aa7e9d0f6635fa24e95794f
Mac <mac@MacBook-Pro-Mac.local>
2024-10-10 23:47:58 +0300

    images

--- app/page.js
+++ app/page.js
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="w-full min-h-screen"></div>;
+  return (
+    <div className="w-full min-h-screen">
+      <Image
+        src="/joker.jpg"
+        width={600}
+        height={600}
+        objectFit="cover"
+        alt="joker"
+      />
+    </div>
+  );
 }
--- app/video/page.jsx
+++ app/video/page.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const page = () => {
-  return <div className="bg-red-500 w-full min-h-screen">page</div>;
+  return (
+    <div className="bg-red-500 w-full min-h-screen">
+      <Image
+        src="/time_enemy.jpg"
+        width={1400}
+        height={600}
+        objectFit="cover"
+        alt="joker"
+      />
+    </div>
+  );
 };
 
 export default page;
