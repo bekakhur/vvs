abc7c81bce64ca51446a3cebc589c19c3830a7f4
Mac <mac@MacBook-Pro-Mac.local>
2024-11-13 01:31:30 +0200

    banner shadow fix

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Banner = ({ img }) => {
   return (
     <div>
-      <div className="bg-black shadow-2xl mb-8 relative overflow-hidden shadow-zinc-900 flex items-center justify-center w-full h-[60vh] sm:h-[100vh]">
+      <div className="bg-black  relative overflow-hidden flex items-center justify-center w-full h-[60vh] sm:h-[100vh]">
         <Image
           src={img}
           width={500}
@@ -23,6 +23,7 @@ const Banner = ({ img }) => {
           WATCH NOW
         </Link>
       </div>
+      <div className="w-full h-20 sm:h-24 bg-gradient-to-t from-transparent via-black via-80% to-black"></div>
     </div>
   );
 };
